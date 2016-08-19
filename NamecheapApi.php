bbec86900ba94f3472a0862fe033ea4ca22dc193
ArturGrigio <fsastudent@yahoo.com>
2016-08-18 18:44:49 -0700

    first commit

---
+++ NamecheapApi.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+
+class NamecheapApi extends Controller
+{
+        public function search($domain) {
+
+        }
+
+        public function buy($domain) {
+
+        }
+
+        private function setDefault() {
+
+        }
+}
+
